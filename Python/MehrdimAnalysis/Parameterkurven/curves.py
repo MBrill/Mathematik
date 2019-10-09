0d7dd86bf4c7d2c586a0aa6317dcaa8d2be69fa6
Manfred Brill <manfred.brill@hs-kl.de>
2019-10-09 11:29:18 +0200

    Grafische Ausgabe für Aufgabe 1
    Blatt Ebene Parameterkurven.

--- Python/MehrdimAnalysis/Parameterkurven/curves.py
+++ Python/MehrdimAnalysis/Parameterkurven/curves.py
@@ -18,3 +18,17 @@ def first_example(t) :
     x = t*t-2.0*t
     y = t + 1.0
     return x, y
+
+""" Aufgabe 1a), Blatt Ebene Parameterkurven """
+def aufgabe1a(t) :
+    t2 = t*t
+    x = 3.0*(t2-3.0)
+    y = t2*t-3.0*t
+    return x, y
+
+""" Aufgabe 1b), Blatt Ebene Parameterkurven """
+def aufgabe1b(t) :
+    t3 = t*t*t
+    x = t3*t - 1.0
+    y = t3 + 1.0
+    return x, y
